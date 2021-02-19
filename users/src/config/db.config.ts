d4e475b17ae28223f80866e964b072b374c64271
Supasin <palsp2406@gmail.com>
2021-02-19 18:55:39 +0700

    Update db.config.ts

--- users/src/config/db.config.ts
+++ users/src/config/db.config.ts
@@ -1,8 +1,8 @@
 export default {
-    host: "localhost",
-    //host: "users-mysql-srv",  // for production
+    // host: "localhost",
+    host: "users-mysql-srv",  // for production
     user: "root",
-    password: "liulaks123",
-    // password: "password",
+    // password: "liulaks123",
+    password: "password",
     db: "users",
 }
