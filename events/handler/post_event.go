55577c603e36b0d0e7c75c23b42a009473b47dcd
Supasin <palsp2406@gmail.com>
2021-03-19 15:49:21 +0700

    Add post book route

--- events/handler/post_event.go
+++ events/handler/post_event.go
@@ -6,6 +6,7 @@ import (
 	"time"
 
 	"github.com/gin-gonic/gin"
+	"github.com/palsp/cuconnex/events/models"
 )
 
 type EventBody struct {
@@ -27,21 +28,45 @@ type Time struct {
 	Hour int  `json:"hour"`
 	Minute int `json:"minute"`
 	Second int `json:"second"`
+	NanoSecond int `json:"nanosecond,omitempty"`
 }
 
+// GetDate returns date in a yyyy-mm-dd hh:mm:ss + nsec format
+func ( d Date ) GetDate(loc time.Location) time.Time{
+	return time.Date(d.Year, d.Month, d.Day , d.Time.Hour ,d.Time.Minute , d.Time.Second, d.Time.NanoSecond, &loc)
+}
 
 // CreateEvent handles the POST
 func CreateEvent(c *gin.Context){
 	postBody := EventBody{}
+
+	// Pase body to struct
 	err := c.BindJSON(&postBody)
 	if err != nil {	
 		fmt.Println(err)
 		c.JSON(http.StatusBadRequest,gin.H{
 			"errors" : err.Error(),
 		})
-	}else{
-		fmt.Println(postBody.StartDate)
-		c.JSON(http.StatusOK, gin.H{})
+		return 
 	}
-	
+
+	// Create a valid entity for event
+	var event = &models.Event{}
+	event.EventName = postBody.EventName
+	event.Description = postBody.Description
+	// fmt.Println(postBody.StartDate.GetDate(postBody.Location))
+	event.Location = postBody.Location.String()
+	if postBody.StartDate != nil {
+		event.StartDate = postBody.StartDate.GetDate(postBody.Location)
+	}
+	if postBody.EndDate != nil {
+		event.EndDate = postBody.EndDate.GetDate(postBody.Location)
+	}
+
+
+
+	models.DB.Create(&event)
+
+	c.JSON(http.StatusCreated, event)
+
 }
--- events/models/event.go
+++ events/models/event.go
@@ -11,9 +11,11 @@ type Event struct {
 	ID uint `json:"id" gorm:"primaryKey"` 
 	EventName string `json:"event-name"`
 	Description string `json:"description"`
-	Location string `json:"location"`
+	Location string`json:"location"`
 	StartDate time.Time `json:"start-date,omitempty"`
 	EndDate time.Time `json:"end-date,omitempty"`
+	CreatedAt time.Time `json:"-"`
+	UpdatedAt time.Time `json:"-"`
 }
 
 func (Event) TableName() string {
