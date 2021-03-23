95e538b3e024a98eb734a112bcc304f47b0c7c45
Pattradanai Punvichartkul <saint12302@gmail.com>
2021-03-23 18:28:01 +0700

    update listener

--- notification/src/listener.ts
+++ notification/src/listener.ts
@@ -10,8 +10,22 @@ const stan: nats.Stan = nats.connect('connex', randomBytes(4).toString('hex'), {
 stan.on('connect', () => {
     console.log('listener connected');
 
-    const subscription = stan.subscribe('friend:added');
+    stan.on('close', process.exit);
+
+    const options: nats.SubscriptionOptions = stan
+        .subscriptionOptions()
+        .setManualAckMode(true)
+        .setAckWait(1000);
+    const subscription = stan.subscribe(
+        'friend:added',
+        'friendServiceQueueGroup',
+        options
+    );
     subscription.on('message', (msg: Message) => {
-        console.log(`message received: ${msg.getData()}`);
+        console.log(`message received: ${msg.getData()}, number: ${msg.getSequence()}`);
+        msg.ack();
     });
 });
+
+process.on('SIGINT', stan.close);
+process.on('SIGTERM', stan.close);
