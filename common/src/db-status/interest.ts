f75c3cdfdac1194f1f098fff98736a513df103de
Supasin <palsp2406@gmail.com>
2021-03-28 19:04:55 +0700

    update interest description

--- common/src/db-status/interest.ts
+++ common/src/db-status/interest.ts
@@ -1,5 +1,45 @@
-export enum InterestDescription {
-    Developer = "Developer",
-    Startup = "Startup",
-    Business = "Business",
+// export enum InterestDescription {
+//     Developer = "Developer",
+//     Startup = "Startup",
+//     Business = "Business",
+// }
+
+/**
+ * Technology interest Description
+ */
+export enum Technology {
+    Coding = "Coding",
+    WebBuilder = "Web Builder",
+    ChatBot = "ChatBot",
+    FinTech = "FinTech"
 }
+
+/**
+ * Business interest Description
+ */
+export enum Business {
+    Marketing = "Marketing",
+    BusinessCase = "Business Case",
+    Startup = "Startup",
+    Ecommerce = "Ecommerce",
+}
+
+/**
+ * Design interest description
+ */
+export enum Design {
+    Graphic = "Graphic",
+    UXUI = "UXUI",
+    Ads = "Ads",
+    Fashion = "Fashion",
+}
+
+/**
+ * Description for interest database
+ */
+export const InterestDescription = {
+    Business: Business,
+    Technology: Technology,
+    Design: Design,
+}
+
