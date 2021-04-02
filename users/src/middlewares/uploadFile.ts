4c49f573826c7783c8a40e123157b119535767e1
AlphaNiner1415 <manchesternd@gmail.com>
2021-04-02 18:37:22 +0700

    Defined file interface

---
+++ users/src/middlewares/uploadFile.ts
@@ -0,0 +1,8 @@
+import { NotAuthorizedError } from '@cuconnex/common';
+import { Request, Response, NextFunction } from 'express';
+import { User } from '../models/user.model';
+export const uploadFile = async (req: Request, res: Response, next: NextFunction) => {
+    if(!req.file)
+
+    next();
+};
