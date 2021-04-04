087159ca8a7220d1e80793b0680b4fbb42d6bee0
AlphaNiner1415 <manchesternd@gmail.com>
2021-04-04 15:30:28 +0700

    Changed file naming system

--- users/src/config/multer.config.ts
+++ users/src/config/multer.config.ts
@@ -1,12 +1,19 @@
 import multer from 'multer';
+import { BadRequestError } from '@cuconnex/common';
 
 //Storage config
 export const storage = multer.diskStorage({
-    destination: function (req, file, cb) {
-        cb(null, './assets')
-    },
+    destination: 'assets',
     filename: function (req, file, cb) {
-        cb(null, file.originalname)
+        console.log(req.body);
+        let extension = '.'+ file.mimetype.split('/')[1];
+        let fileName;
+        if(req.body.name){
+            fileName = req.body.name + "_profile_pic";
+        } else {
+            fileName = "Unknown_profile_pic";
+        }
+        cb(null, fileName + extension)
     }
 })
 
@@ -18,7 +25,7 @@ const fileFilter = (req: any, file: any, cb: any) => {
 
         cb(null, true);
     } else {
-        cb(new Error("Image uploaded is not of type jpg/jpeg or png"),false);
+        cb(new BadRequestError("Image uploaded is not of type jpg/jpeg or png"),false);
     }
 }
 
