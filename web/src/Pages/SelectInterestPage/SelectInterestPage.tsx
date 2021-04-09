2dc09e21d14483fe37bca67215c5f14dcc92bb46
6131866021 <6131866021@student.chula.ac.th>
2021-04-09 11:06:37 +0700

    fail

--- web/src/Pages/SelectInterestPage/SelectInterestPage.tsx
+++ web/src/Pages/SelectInterestPage/SelectInterestPage.tsx
@@ -1,4 +1,4 @@
-import React, { useState, useEffect } from "react";
+import React, { useState, useEffect, useContext } from "react";
 import axios from "@src/api/axiosInstance/axiosInstance";
 import { Link } from "react-router-dom";
 
