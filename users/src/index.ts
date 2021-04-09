96ec6100ade1dd7a2265bc774a2d4e5ab8732f4e
Supasin <palsp2406@gmail.com>
2021-04-09 22:24:39 +0700

    test build

--- users/src/index.ts
+++ users/src/index.ts
@@ -42,7 +42,7 @@ const start = async () => {
   }
   console.log(__dirname);
   app.listen(3000, () => {
-    console.log('Listening on port 3000..');
+    console.log('Listening on port 3000...');
   });
 };
 
