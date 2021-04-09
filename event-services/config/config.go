563c1a2205395172a7a5d77696f72be87d328db6
Supasin <palsp2406@gmail.com>
2021-04-09 20:20:20 +0700

    Update config.go

--- event-services/config/config.go
+++ event-services/config/config.go
@@ -1,5 +1,7 @@
 package config
 
+import "os"
+
 type DBConfig struct {
 	Name string
 	Host string
@@ -7,19 +9,14 @@ type DBConfig struct {
 	Password string
 }
 
-//var DB = DBConfig{
-//	Name: os.Getenv("DB_NAME"),
-//	Host: os.Getenv("DB_HOST"),
-//	User: os.Getenv("DB_USER"),
-//	Password: os.Getenv("DB_PASSWORD"),
-//}
-
 var DB = DBConfig{
-	Name: "eventdb",
-	Host: "localhost",
-	User: "root",
-	Password: "liulaks123",
+	Name: os.Getenv("DB_NAME"),
+	Host: os.Getenv("DB_HOST"),
+	User: os.Getenv("DB_USER"),
+	Password: os.Getenv("DB_PASSWORD"),
 }
+
+
 
 
 
@@ -28,5 +25,5 @@ var TestDB = DBConfig{
 	Name: "eventdb_test",
 	Host: "localhost",
 	User : "root",
-	Password: "liulaks123",
+	Password: "password",
 }
