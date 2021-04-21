7c808fa463702425feb1f245ff7e8ea9da9e3af9
Ravipas Wangananont <pestobaimon@gmail.com>
2021-04-21 16:50:04 +0700

    moved browser router up one component
    fixed transition not working

--- web/src/App.tsx
+++ web/src/App.tsx
@@ -1,13 +1,7 @@
-import React, { useState, useEffect } from "react";
-import {
-  BrowserRouter,
-  Switch,
-  Route,
-  Redirect,
-  useLocation,
-} from "react-router-dom";
-import { AuthenticatedContext } from "@hooks/AuthenticatedContext";
-import { UserDataContext } from "@hooks/UserDataContext";
+import React, {useState, useEffect} from "react";
+import {BrowserRouter, Switch, Route, Redirect, useLocation} from "react-router-dom";
+import {AuthenticatedContext} from "@hooks/AuthenticatedContext";
+import {UserDataContext} from "@hooks/UserDataContext";
 import {
   AuthPage,
   PersonalInfoPage,
@@ -31,11 +25,13 @@ import PushPage from "@pages/PushPage/PushPage";
 import CreateTeamPage from "@pages/CreateTeamPage/CreateTeamPage";
 import classes from "./App.module.css";
 import CreateTeamPrompt from "@pages/CreateTeamPage/CreateTeamPrompt/CreateTeamPrompt";
-import { IUser } from "@models/index";
+import {IUser} from "@models/index";
 
-import { AnimatePresence } from "framer-motion";
+import {AnimatePresence} from "framer-motion";
 
 const App: React.FC = () => {
+  const location = useLocation();
+
   const [isAuthenticated, setIsAuthenticated] = useState<boolean>(true);
   const [userData, setUserData] = useState<IUser>({
     id: "",
@@ -52,65 +48,49 @@ const App: React.FC = () => {
   const [heightStyle, setHeightStyle] = useState({});
 
   useEffect(() => {
-    setHeightStyle({ height: `${window.innerHeight}px` });
+    setHeightStyle({height: `${window.innerHeight}px`});
   }, []);
 
   const routes = isAuthenticated ? (
-    <BrowserRouter>
-      <AuthenticatedContext.Provider
-        value={{ isAuthenticated, setIsAuthenticated }}
-      >
-        <UserDataContext.Provider value={{ userData, setUserData }}>
-          <AnimatePresence exitBeforeEnter>
-            <Switch>
-              <Route path="/" exact component={AuthPage} />
-
-              <Route
-                path="/selectinterests"
-                exact
-                component={SelectInterestPage}
-              />
-              <Route
-                path="/personalInformation"
-                exact
-                component={PersonalInfoPage}
-              />
-              <Route path="/friendlists" exact component={FriendsPage} />
-              {/* <Route path="/findteams" exact component={FindTeamPage} /> */}
-              {/* <Route path="/recruitmembers" exact component={RecruitMemberPage} /> */}
-              <Route path="/success" exact component={SuccessPage} />
-              <Route path="/landing" exact component={LandingPage} />
-              <Route path="/myteams" exact component={MyTeamPage} />
-              <Route path="/profile" exact component={ProfilePage} />
-              <Route path="/selectevents" exact component={SelectEventPage} />
-              <Route path="/selectteams" exact component={SelectTeamPage} />
-              <Route path="/post" exact component={PushPage} />
-              <Route path="/selectmember" exact component={SelectMemberPage} />
-              <Route path="/createteam" exact component={CreateTeamPage} />
-              <Route path="/teamdetail" exact component={TeamDetail} />
-              <Route path="/explore" exact component={ExplorePage} />
-              <Route path="/notification" exact component={NotificationPage} />
-              <Route path="/testprompt" exact component={CreateTeamPrompt} />
-              <Route path="/test" exact component={TestPage} />
-              <Route path="/" component={LandingPage} />
-            </Switch>
-          </AnimatePresence>
-        </UserDataContext.Provider>
-      </AuthenticatedContext.Provider>
-    </BrowserRouter>
-  ) : (
-    <BrowserRouter>
-      <AuthenticatedContext.Provider
-        value={{ isAuthenticated, setIsAuthenticated }}
-      >
-        <UserDataContext.Provider value={{ userData, setUserData }}>
-          <Switch>
+    <AuthenticatedContext.Provider value={{isAuthenticated, setIsAuthenticated}}>
+      <UserDataContext.Provider value={{userData, setUserData}}>
+        <AnimatePresence exitBeforeEnter>
+          <Switch location={location} key={location.pathname}>
             <Route path="/" exact component={AuthPage} />
-            <Route path="/" render={() => <h1>Nothing to see here!!!</h1>} />
+
+            <Route path="/selectinterests" exact component={SelectInterestPage} />
+            <Route path="/personalInformation" exact component={PersonalInfoPage} />
+            <Route path="/friendlists" exact component={FriendsPage} />
+            {/* <Route path="/findteams" exact component={FindTeamPage} /> */}
+            {/* <Route path="/recruitmembers" exact component={RecruitMemberPage} /> */}
+            <Route path="/success" exact component={SuccessPage} />
+            <Route path="/landing" exact component={LandingPage} />
+            <Route path="/myteams" exact component={MyTeamPage} />
+            <Route path="/profile" exact component={ProfilePage} />
+            <Route path="/selectevents" exact component={SelectEventPage} />
+            <Route path="/selectteams" exact component={SelectTeamPage} />
+            <Route path="/post" exact component={PushPage} />
+            <Route path="/selectmember" exact component={SelectMemberPage} />
+            <Route path="/createteam" exact component={CreateTeamPage} />
+            <Route path="/teamdetail" exact component={TeamDetail} />
+            <Route path="/explore" exact component={ExplorePage} />
+            <Route path="/notification" exact component={NotificationPage} />
+            <Route path="/testprompt" exact component={CreateTeamPrompt} />
+            <Route path="/test" exact component={TestPage} />
+            <Route path="/" component={LandingPage} />
           </Switch>
-        </UserDataContext.Provider>
-      </AuthenticatedContext.Provider>
-    </BrowserRouter>
+        </AnimatePresence>
+      </UserDataContext.Provider>
+    </AuthenticatedContext.Provider>
+  ) : (
+    <AuthenticatedContext.Provider value={{isAuthenticated, setIsAuthenticated}}>
+      <UserDataContext.Provider value={{userData, setUserData}}>
+        <Switch>
+          <Route path="/" exact component={AuthPage} />
+          <Route path="/" render={() => <h1>Nothing to see here!!!</h1>} />
+        </Switch>
+      </UserDataContext.Provider>
+    </AuthenticatedContext.Provider>
   );
 
   return (
--- web/src/Pages/LandingPage/LandingPage.tsx
+++ web/src/Pages/LandingPage/LandingPage.tsx
@@ -1,21 +1,21 @@
-import React, { useState, useContext } from "react";
-import { ProfilePic } from "@smartComponents/index";
+import React, {useState, useContext} from "react";
+import {ProfilePic} from "@smartComponents/index";
 import Hamburger from "@dumbComponents/UI/Hamburger/Hamburger";
-import { ArrowLeft, ArrowRight, Search } from "@dumbComponents/UI/Icons";
+import {ArrowLeft, ArrowRight, Search} from "@dumbComponents/UI/Icons";
 import Mail from "@dumbComponents/UI/Icons/Mail/Mail";
-import { Link } from "react-router-dom";
+import {Link} from "react-router-dom";
 import Background from "../../components/dumbComponents/UI/Background/Background";
 import HamburgerPrompt from "./HamburgerPrompt/HamburgerPrompt";
 import classes from "./LandingPage.module.css";
 import LandingHero from "./Sections/LandingHero";
-import { UserDataContext } from "@hooks/UserDataContext";
+import {UserDataContext} from "@hooks/UserDataContext";
 
-import { motion, AnimatePresence, AnimateSharedLayout } from "framer-motion";
+import {motion, AnimatePresence, AnimateSharedLayout} from "framer-motion";
 
 const LandingPage: React.FC = () => {
   const [clickHamburger, setClickHamburger] = useState<boolean>(false);
   const [hasTeam, setHasTeam] = useState<boolean>(true);
-  const { userData } = useContext(UserDataContext);
+  const {userData} = useContext(UserDataContext);
   const hamburgerClickedHandler = () => {
     setClickHamburger(!clickHamburger);
   };
@@ -45,10 +45,7 @@ const LandingPage: React.FC = () => {
             <Search />
           </Link>
         </div>
-        <div
-          onClick={() => setHasTeam((prev) => !prev)}
-          className={classes.mailDiv}
-        >
+        <div onClick={() => setHasTeam((prev) => !prev)} className={classes.mailDiv}>
           <Mail />
         </div>
         <div onClick={hamburgerClickedHandler} className={classes.hamburgerDiv}>
@@ -76,11 +73,11 @@ const LandingPage: React.FC = () => {
     },
     visible: {
       opacity: 1,
-      transition: { delay: 0.5, duration: 1 },
+      transition: {delay: 0.5, duration: 1},
     },
     exit: {
       x: window.innerWidth,
-      transition: { delay: 1, duaration: 1 },
+      transition: {delay: 1, duaration: 1},
     },
   };
 
@@ -89,7 +86,7 @@ const LandingPage: React.FC = () => {
       variants={containerVariants}
       initial="hidden"
       animate="visible"
-      exit="exit"
+      exit={{opacity: 0}}
       className={classes.main}
     >
       <Background>
