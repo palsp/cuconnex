5b9f7cac6101941e06197aa47487530e916ee9d6
Pichaya Puttekulangkura <micky47561@gmail.com>
2021-04-22 23:20:54 +0700

    Micky: Fix baseURL

--- web/src/api/axiosInstance/axiosInstance.ts
+++ web/src/api/axiosInstance/axiosInstance.ts
@@ -1,7 +1,7 @@
 import axios from "axios";
 
 const axiosInstance = axios.create({
-  baseURL: "http://www.cu-connex.com",
+  baseURL: "https://www.cu-connex.com",
   // baseURL: window.location.origin,
 });
 
