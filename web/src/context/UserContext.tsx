78653bf37a936c82f6a1b14da9fa47fad5bc0b39
Pichaya Puttekulangkura <micky47561@gmail.com>
2021-04-22 22:15:18 +0700

    Micky: Modified UserContext / Added @hooks / Fixed context / Changed API

---
+++ web/src/context/UserContext.tsx
@@ -0,0 +1,63 @@
+import React, { useState, useContext } from "react";
+import { IUser } from "@models/index";
+import { fetchUserDataAPI } from "@api/index";
+
+interface IUserContext {
+  userData: IUser;
+  fetchUserDataHandler: () => Promise<void>;
+  setUserDataHandler: (updatedUserData: any) => void;
+}
+export const UserContext = React.createContext<IUserContext>(
+  {} as IUserContext
+);
+export const UserContextProvider = (props: any) => {
+  const [userData, setUserData] = useState<IUser>({
+    id: "",
+    name: "",
+    interests: {
+      Technology: [],
+      Business: [],
+      Design: [],
+    },
+    faculty: "",
+    image: "",
+  });
+
+  const fetchUserDataHandler = async () => {
+    try {
+      const userData = await fetchUserDataAPI();
+      console.log("SUCCESS fetchDataHandler", userData);
+      setUserData(userData.data);
+    } catch (e) {
+      console.log("fetchDataHandler error", e);
+    }
+  };
+
+  const setUserDataHandler = (updatedUserData: any) => {
+    setUserData((prevState) => ({
+      ...prevState,
+      id: updatedUserData.id || prevState.id,
+      name: updatedUserData.name || prevState.name,
+      interests: {
+        Technology:
+          updatedUserData.interests.Technology ||
+          prevState.interests.Technology,
+        Business:
+          updatedUserData.interests.Business || prevState.interests.Business,
+        Design: updatedUserData.interests.Design || prevState.interests.Design,
+      },
+      faculty: updatedUserData.faculty || prevState.faculty,
+      image: updatedUserData.image || prevState.image,
+    }));
+    console.log("setUserDataHandler data is...", updatedUserData);
+  };
+  return (
+    <UserContext.Provider
+      value={{ userData, fetchUserDataHandler, setUserDataHandler }}
+    >
+      {props.children}
+    </UserContext.Provider>
+  );
+};
+
+export default UserContextProvider;
