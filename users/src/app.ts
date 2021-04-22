c7c00be196217b935844bf0e4e683d4b9f6e2e7b
Supasin <palsp2406@gmail.com>
2021-04-23 00:08:13 +0700

    use manual cors handler

--- users/src/app.ts
+++ users/src/app.ts
@@ -4,18 +4,20 @@ import session from 'cookie-session';
 import { json, urlencoded } from 'body-parser';
 import { currentUser, errorHandling, requireAuth, NotFoundError } from '@cuconnex/common';
 import cors from 'cors';
-import { fetchUser } from './middlewares';
+import { corsHandler, fetchUser } from './middlewares';
 import * as router from './routes';
 import { connectionRouter, userRouter, teamRouter } from './routes';
 require('./config/multer.config');
 
 const app = express();
 
-app.use(cors({
-  allowedHeaders: 'Authorization',
-  methods: ["GET", "PUT", "POST", "DELETE", "OPTIONS"]
-}));
+// app.use(cors({
+//   allowedHeaders: 'Authorization',
+//   methods: ["GET", "PUT", "POST", "DELETE", "OPTIONS"]
+// }));
 app.set('trust proxy', true);
+
+app.use(corsHandler);
 
 app.use(json());
 app.use(urlencoded({ extended: true, limit: "800mb" }));
