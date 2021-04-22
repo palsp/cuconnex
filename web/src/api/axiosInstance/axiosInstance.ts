5b763438a431c3ce3290be80400b268d549a2133
Supasin <palsp2406@gmail.com>
2021-04-22 22:50:29 +0700

    Update axiosInstance.ts

--- web/src/api/axiosInstance/axiosInstance.ts
+++ web/src/api/axiosInstance/axiosInstance.ts
@@ -1,7 +1,5 @@
 import axios from "axios";
 
-const token = "eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpZCI6IjYxM3h4eHh4MjEiLCJpYXQiOjE2MTkxMDYwNjR9.8Ne0heTtkpGi7zAMq8YZ4eRH_rxIrBhMF4hkMPTFO5U";
-
 const axiosInstance = axios.create({
   baseURL: "https://www.cu-connex.com",
   // baseURL: window.location.origin,
