5beda59b541fabdc2a2e0ad2e5f6869e97a0cab6
Supasin <palsp2406@gmail.com>
2021-04-23 00:36:59 +0700

    fix bearer token bug

--- common/build/middlewares/requireAuth.d.ts
+++ common/build/middlewares/requireAuth.d.ts
@@ -1,2 +1,2 @@
 import { Request, Response, NextFunction } from 'express';
-export declare const requireAuth: (req: Request, res: Response, next: NextFunction) => Response<any, Record<string, any>> | undefined;
+export declare const requireAuth: (req: Request, res: Response, next: NextFunction) => void;
