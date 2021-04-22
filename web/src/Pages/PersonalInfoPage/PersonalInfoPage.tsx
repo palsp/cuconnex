e6b9ef9f4a22d21808f27ef67867c65f2fefa84d
climaxhome <57805640+climaxhome@users.noreply.github.com>
2021-04-22 23:30:43 +0700

    Home: Calculate studentYear and studentFaculty
    - Calculate studentYear and studentFaculty based on entered student id
    - Send Props of studentYear and studentFaculty to PersonalInformationPage

--- web/src/Pages/PersonalInfoPage/PersonalInfoPage.tsx
+++ web/src/Pages/PersonalInfoPage/PersonalInfoPage.tsx
@@ -47,11 +47,20 @@ const facultyArray: string[] = Object.values(FacultyListsEnum);
 //   "Agricultural Resources",
 // ];
 
+interface Props {
+  location: {
+    state: {
+      year: number;
+      faculty: FacultyListsEnum;
+    };
+  };
+}
+
 const validationSchema = yup.object({
   displayName: yup.string().required("Display name is required"),
 });
 
-const PersonalInfoPage: React.FC = () => {
+const PersonalInfoPage: React.FC<Props> = (props) => {
   const [redirect, setRedirect] = useState<JSX.Element>();
   const [imagePreview, setImagePreview] = useState<string>("");
   const [imageRaw, setImageRaw] = useState<File>();
