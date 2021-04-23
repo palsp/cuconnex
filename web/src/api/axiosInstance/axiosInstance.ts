3c525cc1bfeb7073aec0b2335d461cb3c2016387
Pichaya Puttekulangkura <micky47561@gmail.com>
2021-04-23 16:44:56 +0700

    Micky: Updated logo

--- web/src/api/axiosInstance/axiosInstance.ts
+++ web/src/api/axiosInstance/axiosInstance.ts
@@ -1,7 +1,7 @@
 import axios from "axios";
 
-
-const token = "eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpZCI6IjYxM3h4eHh4MjEiLCJpYXQiOjE2MTkxMTAxNTZ9.PufmDhczbRfppx_c9BkbCnT-_bdW0qHaECK7M-SZ_7o";
+const token =
+  "eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpZCI6IjYxM3h4eHh4MjEiLCJpYXQiOjE2MTkxMTAxNTZ9.PufmDhczbRfppx_c9BkbCnT-_bdW0qHaECK7M-SZ_7o";
 const axiosInstance = axios.create({
   baseURL: "https://www.cu-connex.com",
   // baseURL: window.location.origin,
