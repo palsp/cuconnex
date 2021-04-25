04a98f614106cfc366323c505d9c556d45ff71b5
Ravipas Wangananont <pestobaimon@gmail.com>
2021-04-25 16:27:03 +0700

    CSS fixes

--- web/src/api/axiosInstance/axiosInstance.ts
+++ web/src/api/axiosInstance/axiosInstance.ts
@@ -1,12 +1,12 @@
 import axios from "axios";
 
-const token =
-  "eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpZCI6IjYxMjQ1Nzg4MjEiLCJpYXQiOjE2MTkzNDEwMjR9._vQjzoH36vz37Klc6Q4TgQHzednGVrMQD9JKdzAqDQk";
+// const token =
+//   "eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpZCI6IjYxMjQ1Nzg4MjEiLCJpYXQiOjE2MTkzNDIwMjJ9.HP7GRu9cjIfWWrqSx_bWlwNkjmMvq-e9vz7XDbXKgeU";
 const axiosInstance = axios.create({
   baseURL: "https://www.cu-connex.com",
   // baseURL: window.location.origin,
 });
 
-axiosInstance.defaults.headers.common["Authorization"] = "Bearer " + token;
+// axiosInstance.defaults.headers.common["Authorization"] = "Bearer " + token;
 
 export default axiosInstance;
