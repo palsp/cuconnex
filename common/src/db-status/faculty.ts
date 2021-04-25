b3117e933ac1777ed0fc8a509771fb4ec0558cf0
Supasin <palsp2406@gmail.com>
2021-04-25 20:19:11 +0700

    add faculty

--- common/src/db-status/faculty.ts
+++ common/src/db-status/faculty.ts
@@ -1,26 +1,26 @@
-export enum Faculty {
-  AlliedHealthSciences = 'Allied Health Sciences', // สหเวช
-  Architecture = 'Architecture',
-  Arts = 'Arts',
-  CommunicationArts = 'Communication Arts',
-  CommerceAndAccountancy = 'Commerce and Accountancy', // บัญชี
-  Dentistry = 'Dentistry',
-  Economics = 'Economics',
-  Education = 'Education', // ครุศาสตร์
-  Engineering = 'Engineering',
-  FineAndAppliedArts = 'Fine and Applied Arts', // ศิลปกรรม
-  Law = 'Law',
-  Medicine = 'Medicine',
-  Nursing = 'Nursing',
-  PharmaceuticalSciences = 'Pharmaceutical Sciences',
-  PoliticalSciences = 'Political Sciences',
-  Psychology = 'Psychology',
-  Science = 'Science',
-  SportsScience = 'Sports Science',
-  VeterinaryScience = 'VeterinaryScience',
-  IntegratedInnovation = 'Integrated Innovation', // วัฒนกรรมบูรณาการ
-  AgriculturalResources = 'Agricultural Resources'
-}
+// export enum Faculty {
+//   AlliedHealthSciences = 'Allied Health Sciences', // สหเวช
+//   Architecture = 'Architecture',
+//   Arts = 'Arts',
+//   CommunicationArts = 'Communication Arts',
+//   CommerceAndAccountancy = 'Commerce and Accountancy', // บัญชี
+//   Dentistry = 'Dentistry',
+//   Economics = 'Economics',
+//   Education = 'Education', // ครุศาสตร์
+//   Engineering = 'Engineering',
+//   FineAndAppliedArts = 'Fine and Applied Arts', // ศิลปกรรม
+//   Law = 'Law',
+//   Medicine = 'Medicine',
+//   Nursing = 'Nursing',
+//   PharmaceuticalSciences = 'Pharmaceutical Sciences',
+//   PoliticalSciences = 'Political Sciences',
+//   Psychology = 'Psychology',
+//   Science = 'Science',
+//   SportsScience = 'Sports Science',
+//   VeterinaryScience = 'VeterinaryScience',
+//   IntegratedInnovation = 'Integrated Innovation', // วัฒนกรรมบูรณาการ
+//   AgriculturalResources = 'Agricultural Resources'
+// }
 
 export const faculty = {
   "01": "THE SIRINDHORN THAI LANGUAGE INSTITUTE",
@@ -34,5 +34,22 @@ export const faculty = {
   "26": "FACULTY OF COMMERCE AND ACCOUNTANCY",
   "27": "FACULTY OF EDUCATION",
   "28": "FACULTY OF COMMUNICATION ARTS",
-  "29": "FACULTY OF ECONOMICS"
+  "29": "FACULTY OF ECONOMICS",
+  "30": "FACULTY OF MEDICINE",
+  "31": "FACULTY OF VETERINARY SCIENCE",
+  "32": "FACULTY OF DENTISTRY",
+  "33": "FACULTY OF PHARMACEUTICAL SCIENCES",
+  "34": "FACULTY OF LAW",
+  "35": "FACULTY OF FINE AND APPLIED ARTS",
+  "36": "FACULTY OF NURSING",
+  "37": "FACULTY OF ALLIED HEALTH SCIENCES",
+  "38": "FACULTY OF PSYCHOLOGY",
+  "39": "FACULTY OF SPORTS SCIENCE",
+  "40": "SCHOOL OF AGRICULTURAL RESOURCES",
+  "51": "COLLEGE OF POPULATION STUDIES",
+  "53": "COLLEGE OF PUBLIC HEALTH SCIENCES",
+  "55": "LANGUAGE INSTITUTE",
+  "56": "SCHOOL OF INTEGRATED INNOVATION",
+  "58": "SASIN GRADUATE INSTITUTE OF BUSINESS ADMINISTION",
+  "99": "OTHER UNIVERSITY"
 }
