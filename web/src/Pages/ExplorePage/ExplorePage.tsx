07810e8fbf1715b5cb3f2c8d5d3bad615d04a7fd
Pichaya Puttekulangkura <micky47561@gmail.com>
2021-04-26 23:59:37 +0700

    Micky: Removed unused var

--- web/src/Pages/ExplorePage/ExplorePage.tsx
+++ web/src/Pages/ExplorePage/ExplorePage.tsx
@@ -1,4 +1,4 @@
-import React, { useContext, useEffect, useState } from "react";
+import React, { useState } from "react";
 import classes from "./ExplorePage.module.css";
 import {
   ActivityBoxes,
@@ -8,37 +8,22 @@ import {
   SearchBar,
 } from "@smartComponents/index";
 import { ArrowLeft } from "@icons/index";
-import { Background, Heading, Subtitle, Tag } from "@dumbComponents/UI";
+import { Heading, Subtitle, Tag } from "@dumbComponents/UI";
 import { Link } from "react-router-dom";
 import mockEventLists from "@src/mockData/mockEventLists";
 import mockMyTeamListsData from "@src/mockData/mockMyTeamListsData";
 import mockActivityBoxes from "@src/mockData/mockActivitiesBoxes";
-import { mockPeopleLists } from "@src/mockData";
 import { IEventData, ITeam, IUser } from "@src/models";
 import { motion } from "framer-motion";
 import containerVariants from "@src/models/models";
-import { callTeamOfUserAPI } from "@src/api";
-import { UserContext } from "@context/UserContext";
 
 const ExplorePage = () => {
   const [hasSearch, setHasSearch] = useState<boolean>(false);
   const [noSearchResult, setNoSearchResult] = useState<boolean>(false);
   const [peopleLists, setPeopleLists] = useState<IUser[]>([]);
   const [teamLists, setTeamLists] = useState<ITeam[]>([]);
-  const [currentTeamLists, setCurrentTeamLists] = useState<ITeam[]>([]);
   const [eventLists, setEventLists] = useState<IEventData[]>([]);
-  const { userData } = useContext(UserContext);
 
-  useEffect(() => {
-    fetchTeamHandler().then((value: ITeam[] | []) =>
-      setCurrentTeamLists(value)
-    );
-  }, []);
-  const fetchTeamHandler = async () => {
-    const teamData = await callTeamOfUserAPI(userData.id);
-    console.log("fetchTeamHandler", teamData);
-    return teamData.data.teams;
-  };
   const explorePage = hasSearch ? (
     <div className={classes.exploreContent}>
       <Tag />
