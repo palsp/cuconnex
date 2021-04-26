a9d8ec176c07b7ee48e47e906dbbf7ffd1e108e3
climaxhome <57805640+climaxhome@users.noreply.github.com>
2021-04-26 23:12:15 +0700

    Home: Fixed ActivityBox Background and MemberList Display

--- web/src/components/smartComponents/MemberLists/MemberList/MemberList.tsx
+++ web/src/components/smartComponents/MemberLists/MemberList/MemberList.tsx
@@ -34,17 +34,10 @@ const MemberList: React.FC<Props> = (props) => {
           <ProfilePic PicUrl={props.members.image} />
         </div>
         <div className={classes.userInfo}>
-          <div className={classes.divUserInfo}>
-            <Username value={props.members.name} />
-          </div>
-          <div className={classes.divUserInfo}>
-            <Heading value={props.members.role} size="small" />
-          </div>
-          <div className={classes.divUserInfo}>
-            <Subtitle value={props.members.faculty} />
-          </div>
-          <div className={classes.divUserInfo}>
-            <Subtitle value={props.members.year} />
+          <div className={classes.divUsernameInfo}>{props.members.name}</div>
+          <div className={classes.divRoleInfo}>{props.members.role}</div>
+          <div className={classes.divFacultyInfo}>
+            {props.members.faculty + ", " + props.members.year}
           </div>
         </div>
         <div className={classes.checkboxDiv} onClick={checkedMemberHandler}>
