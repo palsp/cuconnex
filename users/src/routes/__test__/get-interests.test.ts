e1439ac9d2ff1dc0f929118507c15d7fca7fb73e
Pattradanai Punvichartkul <saint12302@gmail.com>
2021-04-26 15:45:22 +0700

    finalize get interests test

--- users/src/routes/__test__/get-interests.test.ts
+++ users/src/routes/__test__/get-interests.test.ts
@@ -1,5 +1,25 @@
 import { app } from '../../app';
 import request from 'supertest';
 
-it('return .......', async () => {
-})
+describe('Get interests', () => {
+	it('should return a list of interests', async () => {
+		const res = await request(app).get('/api/users/interests').send().expect(200);
+		const modelRes = {
+			interests: [
+				{
+					category: 'Business',
+					interests: ['Marketing', 'Business Case', 'Startup', 'Ecommerce'],
+				},
+				{
+					category: 'Technology',
+					interests: ['Coding', 'Web Builder', 'ChatBot', 'FinTech'],
+				},
+				{
+					category: 'Design',
+					interests: ['Graphic', 'UXUI', 'Ads', 'Fashion'],
+				},
+			],
+		};
+        expect(res.body).toEqual(modelRes);
+	});
+});
