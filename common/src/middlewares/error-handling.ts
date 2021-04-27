e93b2359f16fccc2b0fafb5ddc57d1c3d9dd9784
Krittamook Aksornchindarat <krittamook@hotmail.com>
2021-04-27 15:30:54 +0700

    add err message at errorhandling

--- common/src/middlewares/error-handling.ts
+++ common/src/middlewares/error-handling.ts
@@ -1,21 +1,17 @@
-import { Request, Response, NextFunction } from 'express'
+import { Request, Response, NextFunction } from 'express';
 import { CustomError } from '../errors/custom-error';
 
-
-
-
 export const errorHandling = (err: Error, req: Request, res: Response, next: NextFunction) => {
+  // if error is an instacne of Custom error
+  // we return an error message with fix format
+  if (err instanceof CustomError) {
+    return res.status(err.statusCode).send({ errors: err.serializeErrors() });
+  }
 
-    // if error is an instacne of Custom error
-    // we return an error message with fix format
-    if (err instanceof CustomError) {
-        return res.status(err.statusCode).send({ errors: err.serializeErrors() });
-    }
+  // allow developer to debug error
+  console.error(err);
 
-    // allow developer to debug error
-    console.error(err)
-
-    res.status(400).send({
-        errors: [{ message: 'Something Went Wrong' }]
-    })
-}
+  res.status(400).send({
+    errors: [{ message: `Something Went Wrong ${err.message}` }],
+  });
+};
