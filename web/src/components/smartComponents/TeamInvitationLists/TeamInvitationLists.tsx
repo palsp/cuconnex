d830ec5304200856a1059a929a0172e0e02b899a
Pichaya Puttekulangkura <micky47561@gmail.com>
2021-04-29 00:33:14 +0700

    Micky: Fixed TeamNotiPage

--- web/src/components/smartComponents/TeamInvitationLists/TeamInvitationLists.tsx
+++ web/src/components/smartComponents/TeamInvitationLists/TeamInvitationLists.tsx
@@ -14,7 +14,7 @@ const TeamInvitationLists: React.FC<Props> = (props) => {
     <div className={classes.TeamInvitationLists}>
       <div className={classes.listHeader}>Team Invitations</div>
       <div className={classes.list}>
-        {props.teams.map((TeamInvitation: ITeam, index: number) => {
+        {props.teams?.map((TeamInvitation: ITeam, index: number) => {
           return (
             <div key={index}>
               <TeamInvitationList key={index} teams={TeamInvitation} />
