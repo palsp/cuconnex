59d09aee7bfeb2ca55072576a5937acaa680d059
Pichaya Puttekulangkura <micky47561@gmail.com>
2021-04-29 23:20:24 +0700

    Micky: Prepare for Image uploading for team / Fixed Create Team outside Formik

--- web/src/Pages/CreateTeamPage/CreateTeamPrompt/CreateTeamPrompt.tsx
+++ web/src/Pages/CreateTeamPage/CreateTeamPrompt/CreateTeamPrompt.tsx
@@ -13,6 +13,7 @@ import { Link, Redirect } from "react-router-dom";
 import SelectMemberPrompt from "../SelectMemberPrompt/SelectMemberPrompt";
 import classes from "./CreateTeamPrompt.module.css";
 import { ErrorContext } from "@context/ErrorContext";
+import defaultProfilePic from "@assets/tempProfilePic.png";
 interface Props {
   members: IUserFriend[] | [];
 }
@@ -20,11 +21,30 @@ const CreateTeamPrompt: React.FC<Props> = (props) => {
   const [clickSelectMember, setClickSelectMember] = useState<boolean>(false);
   const [clickCreateTeam, setClickCreateTeam] = useState<boolean>(true);
   const [redirect, setRedirect] = useState<boolean>(false);
+  const [imagePreview, setImagePreview] = useState<string>("");
+  const [imageRaw, setImageRaw] = useState<File>();
+
+  const { setErrorHandler } = useContext(ErrorContext);
+
+  const handleUploadedImage = (e: React.ChangeEvent<HTMLInputElement>) => {
+    if (e.target.files?.length) {
+      setImagePreview(URL.createObjectURL(e.target.files[0]));
+      setImageRaw(e.target.files[0]);
+    }
+  };
+  const handleInitialImage = () => {
+    const fileName = "myFile.png";
+    fetch(defaultProfilePic).then(async (response) => {
+      const blob = await response.blob();
+      const file = new File([blob], fileName, { type: "image/png" });
+      setImagePreview(URL.createObjectURL(blob));
+      setImageRaw(file);
+    });
+  };
   const backClickedHandler = () => {
     setClickSelectMember(true);
     setClickCreateTeam(false);
   };
-  const { setErrorHandler } = useContext(ErrorContext);
   const createTeamHandler = async (teamData: ITeamData) => {
     const resultTeam = await createTeamAPI(teamData);
     console.log("Successfully sent a POST request to teams", resultTeam);
@@ -35,7 +55,6 @@ const CreateTeamPrompt: React.FC<Props> = (props) => {
       const resultInvitation = await teamInvitationAPI(inviteData);
       console.log(
         "Hi, Successfully sent a POST request to /api/teams/invite-member",
-
         resultInvitation
       );
       setRedirect(true);
@@ -65,9 +84,9 @@ const CreateTeamPrompt: React.FC<Props> = (props) => {
             </div>
             <div className={classes.newTeamDiv}>New team</div>
             <div>
-              <button className={classes.createDiv} value="Next" type="submit">
+              <label className={classes.createDiv} htmlFor="create-button">
                 Create
-              </button>
+              </label>
             </div>
           </div>
           <Formik
@@ -107,6 +126,11 @@ const CreateTeamPrompt: React.FC<Props> = (props) => {
                     type="input"
                   />
                 </div>
+                <button
+                  id="create-button"
+                  style={{ display: "none" }}
+                  type="submit"
+                ></button>
               </Form>
             )}
           </Formik>
