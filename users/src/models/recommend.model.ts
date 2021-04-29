10249a69e78c504bd72953dbdcf6ff55f9f6cd1a
AlphaNiner1415 <39154768+AlphaNiner1415@users.noreply.github.com>
2021-04-29 18:32:12 +0700

    Added crude recommend table model

---
+++ users/src/models/recommend.model.ts
@@ -0,0 +1,52 @@
+import { Sequelize, Model, DataTypes } from 'sequelize';
+import { TableName } from './types';
+
+interface RecommendAttrs {
+    userId: string;
+    recommenderId: string;
+    score: number;
+}
+
+interface RecommendCreationAttrs {
+    score?: number;
+    userId?: string;
+    recommenderId?: string;
+}
+
+class Recommend extends Model<RecommendAttrs, RecommendCreationAttrs> implements RecommendAttrs {
+    public userId!: string;
+    public recommenderId!: string;
+    public score!: number;
+
+    /**
+     * Automatically migrate  schema, to keep your schema up to date.
+     * @param sequelize 
+     */
+    public static autoMigrate(sequelize: Sequelize) {
+        Recommend.init<Recommend>({
+            recommenderId: {
+                type: DataTypes.STRING(11),
+                primaryKey: true,
+            },
+            userId: {
+                type: DataTypes.STRING(11),
+                primaryKey: true,
+
+            },
+            score: {
+                type: DataTypes.DOUBLE(2,2),
+                defaultValue: 0,
+                allowNull: false,
+            }
+        }, {
+            tableName: TableName.recommendations,
+            sequelize,
+            timestamps: false
+        });
+    }
+
+}
+
+
+
+export { Recommend };
