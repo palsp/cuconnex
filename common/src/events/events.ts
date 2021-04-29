8cbebd579697368cc61fafea25581363f84f1058
Supasin <palsp2406@gmail.com>
2021-04-29 17:51:05 +0700

    add nats pub-sub template

---
+++ common/src/events/events.ts
@@ -0,0 +1,9 @@
+import { Subjects } from './subjects';
+
+export interface EventCreated {
+    subject : Subjects.EventCreated,
+    data : {
+        id : number,
+        "event-name" : string,
+    }
+}
