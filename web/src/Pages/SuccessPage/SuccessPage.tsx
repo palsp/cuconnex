0cc8ecebf11a98372f68b13fe2a38743b3452166
Ravipas Wangananont <pestobaimon@gmail.com>
2021-04-30 11:18:30 +0700

    framer motion bug fix

--- web/src/Pages/SuccessPage/SuccessPage.tsx
+++ web/src/Pages/SuccessPage/SuccessPage.tsx
@@ -5,7 +5,6 @@ import { motion } from "framer-motion";
 import containerVariants from "@src/models/models";
 
 import {
-  AppLogo,
   Background,
   Button,
   Subtitle,
@@ -16,6 +15,10 @@ import {
 import classes from "./SuccessPage.module.css";
 
 const SuccessPage: React.FC = () => {
+  const variants = {
+    visible: { y: 0 },
+    hidden: { y: 100 },
+  };
   return (
     <motion.div
       variants={containerVariants}
@@ -25,44 +28,45 @@ const SuccessPage: React.FC = () => {
       className={classes.background}
     >
       <Background data-test="auth-page-background">
-              <motion.div
-                // animate={{y: -window.innerHeight*0.2}}
-                animate={{ y: -(window.innerHeight * 0.1) }}
-                className={classes.contentContainer}>
-              <div className={classes.logoDiv}>
-                <Logo />
-              </div>
-              <motion.div
-                transition={{ ease: "linear", duration: 4, repeat: Infinity }}
-                style={{ bottom: -(window.innerHeight * 0.33) }}
-                className={classes.circle_overlay}
-                data-test="success-page-halfcircleoverlay"
-              ></motion.div>
-              <div className={classes.divHeader}>
-                <Heading
-                  data-test="success-page-header"
-                  value="Welcome Suki!"
-                />
-                <Subtitle
-                  data-test="success-page-subtitle"
-                  value="Your sign up is successful"
-                />
-              </div>
+        <motion.div
+          // animate={{y: -window.innerHeight*0.2}}
+          // animate={{ y: -(window.innerHeight * 0.1) }}
+          initial="hidden"
+          animate="visible"
+          variants={variants}
+          className={classes.contentContainer}
+        >
+          <div className={classes.logoDiv}>
+            <Logo />
+          </div>
+          <motion.div
+            transition={{ ease: "linear", duration: 4, repeat: Infinity }}
+            style={{ bottom: -(window.innerHeight * 0.33) }}
+            className={classes.circle_overlay}
+            data-test="success-page-halfcircleoverlay"
+          ></motion.div>
+          <div className={classes.divHeader}>
+            <Heading data-test="success-page-header" value="Welcome Suki!" />
+            <Subtitle
+              data-test="success-page-subtitle"
+              value="Your sign up is successful"
+            />
+          </div>
 
-              <div className={classes.check}>
-                <Check circle={true} />
-              </div>
+          <div className={classes.check}>
+            <Check circle={true} />
+          </div>
 
-              <Link to="/landing">
-                <div className={classes.button}>
-                  <Button
-                    data-test="success-page-button"
-                    value="Let's get start"
-                  ></Button>
-                </div>
-              </Link>
-              </motion.div>
-          </Background>
+          <Link to="/landing">
+            <div className={classes.button}>
+              <Button
+                data-test="success-page-button"
+                value="Let's get start"
+              ></Button>
+            </div>
+          </Link>
+        </motion.div>
+      </Background>
     </motion.div>
   );
 };
