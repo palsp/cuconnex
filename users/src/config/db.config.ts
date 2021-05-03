88543900145980897f04e3c3994195a5f9b05f05
Supasin <palsp2406@gmail.com>
2021-05-03 13:28:53 +0700

    update user db config

--- users/src/config/db.config.ts
+++ users/src/config/db.config.ts
@@ -1,21 +1,15 @@
-// export default {
-//   host: process.env.DB_HOST,
-//   user: process.env.DB_USER,
-//   password: process.env.DB_PASSWORD,
-//   db: process.env.DB_SCHEMA,
-// };
-
 export default {
-  host: 'localhost',
-  user: 'root',
-  password: 'liulaks123',
-  db: 'user_db',
+  host: process.env.DB_HOST,
+  user: process.env.DB_USER,
+  password: process.env.DB_PASSWORD,
+  db: process.env.DB_SCHEMA,
 };
+
 
 /*TODO: change this back to default settings after development*/
 export const test_config = {
   host: 'localhost',
   user: 'root',
-  password: 'liulaks123',
+  password: 'password',
   db: 'testdb',
 };
