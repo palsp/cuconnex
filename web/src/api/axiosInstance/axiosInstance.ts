def988865ccac5bc57848b49bdfa1e939a316032
Supasin <palsp2406@gmail.com>
2021-05-04 11:08:16 +0700

    fix event cors

--- web/src/api/axiosInstance/axiosInstance.ts
+++ web/src/api/axiosInstance/axiosInstance.ts
@@ -1,7 +1,7 @@
 import axios from "axios";
 
 // const token =
-//   "eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpZCI6IjYxMzIzNDMzMzEiLCJpYXQiOjE2MjAwNTA1Mzh9.EpIwHDkfyfJl9qiSnpXXRgDUxOaNTnNpF6ALWkVW8YY";
+//   "eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpZCI6IjYxMzE3NzY2MjEiLCJpYXQiOjE2MjAxMDA4MzF9.XOg5R7aoWoAII4JKe69vitbY2SJljclkaNf2AjKONnM";
 
 const axiosInstance = axios.create({
   baseURL: "https://www.cu-connex.com",
@@ -18,7 +18,8 @@ export default axiosInstance;
 //"eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpZCI6IjYyMzMyMzIzMjEiLCJpYXQiOjE2MjAwMjY0MDd9.84prTWP7OlkJBsD2VHV0jIfEzSUtbVyRAVXCGKFTAbk"
 
 //loong01
-//"eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpZCI6IjYxMzE3MzYzMjEiLCJpYXQiOjE2MTk3OTc0MTJ9.BeMcj-CogN3IewAwd7ekZMwJIHebenhIw9IA3DmA5Rg"
+
+// "eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpZCI6IjYxMzE3MzYzMjEiLCJpYXQiOjE2MTk3OTc0MTJ9.BeMcj-CogN3IewAwd7ekZMwJIHebenhIw9IA3DmA5Rg";
 //"eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpZCI6IjYxMzcyNjM3MjEiLCJpYXQiOjE2MTk3NzQwMDd9.TvE3aIPUcjI8K068fNPkPKtFaveFK7gBoiENwlM8jbE"
 //"eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpZCI6IjYxMjM3MjgzMjMiLCJpYXQiOjE2MTk3NjQyMjd9.107PhLfTC8EndfbwuN8FilKW7qAX2uthS60bW6VKhZo"
 //"eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpZCI6IjYxMzcyODM3MjMiLCJpYXQiOjE2MTk3NjQxMjJ9.SG1ojaQWwckAVYmu5qSCzIpR3N8Y76cdhA9JERPNcgo"
