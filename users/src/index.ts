df62ed05ba5eb0a9de4b24ffc6ef0bf737a7d8cd
Supasin <palsp2406@gmail.com>
2021-05-05 00:59:21 +0700

    Update index.ts

--- users/src/index.ts
+++ users/src/index.ts
@@ -65,6 +65,9 @@ const start = async () => {
     // initial data for interest and category
     // it should be run only once
     await startDB();
+
+
+    
     await insertFaculties();
     
   // console.log(u)
