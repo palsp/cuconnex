c9f93809ece1609204a16d2861f4263b1ade8aac
Pichaya Puttekulangkura <micky47561@gmail.com>
2021-05-04 20:23:32 +0700

    Micky: AxiosInstance

--- web/src/api/axiosInstance/axiosInstance.ts
+++ web/src/api/axiosInstance/axiosInstance.ts
@@ -1,14 +1,14 @@
 import axios from "axios";
 
-// const token =
-//   "eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpZCI6IjYxMzMzNDQ0MjEiLCJpYXQiOjE2MjAxMjA1MDd9.BMhRZ4M2hKC5EdyjBZ66kI61IWrLGx6TZqbn20RG-4U";
+const token =
+  "eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpZCI6IjYxMzMzNDQ0MjEiLCJpYXQiOjE2MjAxMjA1MDd9.BMhRZ4M2hKC5EdyjBZ66kI61IWrLGx6TZqbn20RG-4U";
 
 const axiosInstance = axios.create({
-  // baseURL: "https://www.cu-connex.com",
-  baseURL: window.location.origin,
+  baseURL: "https://www.cu-connex.com",
+  // baseURL: window.location.origin,
 });
 
-// axiosInstance.defaults.headers.common["Authorization"] = "Bearer " + token;
+axiosInstance.defaults.headers.common["Authorization"] = "Bearer " + token;
 export default axiosInstance;
 
 //in some teams
