56e67dd01fdeab1e6023fb1e9cd72e93a4a939cc
Krittamook Aksornchindarat <krittamook@hotmail.com>
2021-05-04 13:03:11 +0700

    add table faculty

---
+++ users/src/models/faculty.model.ts
@@ -0,0 +1,55 @@
+import { DataTypes, Model, Sequelize } from 'sequelize';
+import { TableName } from './types';
+
+interface FacultyAttrs {
+  code: string;
+  name: string;
+  image: string;
+}
+
+interface FacultyCreationAttrs {
+  code: string;
+}
+
+class Faculty extends Model<FacultyAttrs, FacultyCreationAttrs> {
+  code!: string;
+  name!: string;
+  image!: string;
+
+  public static autoMigrate(sequelize: Sequelize): void {
+    Faculty.init(
+      {
+        code: {
+          type: DataTypes.STRING(2),
+          primaryKey: true,
+        },
+        name: {
+          type: DataTypes.STRING(255),
+          allowNull: false,
+        },
+        image: {
+          type: DataTypes.STRING(255),
+          allowNull: true,
+        },
+      },
+      {
+        tableName: TableName.faculty,
+        sequelize,
+        timestamps: false,
+        version: true,
+      }
+    );
+  }
+
+  // public toJSON(): IEventResponse {
+  //   const values = { ...this.get() };
+  //   let candidate: ITeamResponse[] = [];
+  //   if (this.candidate) {
+  //     candidate = this.candidate.map((c) => c.toJSON());
+  //   }
+
+  //   return { ...values, candidate };
+  // }
+}
+
+export { Faculty };
